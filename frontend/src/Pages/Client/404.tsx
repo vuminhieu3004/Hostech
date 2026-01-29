6e93d67a3da554a3af7186b450c4cc712e9af166
Quý <phungxuanquy24721@gmail.com>
2026-01-29 19:09:49 +0700

    - Cập nhật giao diện trang 404

--- frontend/src/Pages/Client/404.tsx
+++ frontend/src/Pages/Client/404.tsx
@@ -3,22 +3,33 @@ import { Link } from "react-router";
 
 const Notfound = () => {
   return (
-    <section className="page_404">
+    <section className="mt-15">
       <div className="container">
         <div className="row">
           <div className="col-sm-12 ">
             <div className="col-sm-10 col-sm-offset-1  text-center">
-              <div className="four_zero_four_bg">
-                <h1 className="text-center ">404</h1>
+              <div
+                className="h-100 bg-center bg-no-repeat"
+                style={{
+                  backgroundImage:
+                    "url('https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif')",
+                }}
+              >
+                <h1 className="text-center font-bold text-6xl text-red-500">
+                  404
+                </h1>
               </div>
 
               <div className="contant_box_404">
-                <h3 className="h2">Look like you're lost</h3>
+                <h3 className="h2">Không tìm thấy trang phù hợp</h3>
 
-                <p>the page you are looking for not avaible!</p>
+                <p className="mb-5">Vui long quay lại!</p>
 
-                <Link to="/auth" className="link_404">
-                  Go to Home
+                <Link
+                  to="/auth"
+                  className="p-2 text-white hover:font-bold bg-green-600 drop-shadow drop-shadow-red-600"
+                >
+                  Chuyển đến đăng nhập
                 </Link>
               </div>
             </div>
